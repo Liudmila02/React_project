62cf68d841b05fcc21ea301ca012684e0f78750b
Liudmila02 <vasulivna.02@gmail.com>
2019-10-11 18:02:52 +0300

    completed

--- src/components/tasks/taskList.js
+++ src/components/tasks/taskList.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment} from "react";
+import React, { Component, Fragment, useCallback} from "react";
 import {request} from '../../utils/axios';
 import {Link} from 'react-router-dom'
 import '../../style/nav.css'
@@ -39,39 +39,73 @@ class TaskList extends Component {
       });
 }
 
-  completedItem(){
-    request.put(`/api/tasks/${this.props.match.params.taskId}`)
-        .then(res => {
-          console.log(res);
-          console.log(res.data);
+// completedItem(){
+//   request.put(`/api/tasks/${this.props.match.params.taskId}`, {completed: this.state.completed == true} )
+//     .then(res => {
+//       this.setState({
+        
+//       })
+//       console.log("button clicked")
+//     }).catch(function (err) {
+//       console.log(err.res);
+//     })
+// }
 
-          this.setState({update: res.data.task.completed})
-          // window.location.href='/tasks'
-        }).catch(function (err) {
-          console.log(err.res);
-        })
+  // completedItem(){
+
+  //   request.put(`/api/tasks/${this.props.match.params.taskId}`, {completed: this.state.completed})
+  //       .then(res => {
+  //         //console.log(res);
+  //         // console.log(res.data);
+          
+  //         // window.location.href='/tasks'
+  //       }).catch(function (err) {
+  //         console.log(err.res);
+  //       })
     
-  }
-  
+  // }
   // completedItem() {
-  //   if (id == task.id) {
-  //     if (task.completed){
-  //         task.completed = false; 
-  //     }
-  //     else if (!task.completed) {
-  //         task.completed = true;  
-  //     }
-  //   console.log(this.props)
-  //   request.put(`/api/tasks/${this.props.match.params.taskId}`)
+  //   // const completed = false
+  //   request.put(`/api/tasks/${this.props.match.params.taskId}`, { completed: true})
   //     .then(res => {
-  //       console.log(res);
+  //       console.log('!!!!!!!!!!!!!')
+  //       // console.log(res);
   //       console.log(res.data);
-  //       window.location.href='/tasks'
-  //     }).catch(function (err) {
-  //       console.log(err.res);
+
+      
+  //       // this.setState({
+  //       // update: completed == true
+  //       // })
+  //     })
+  //     .catch(function (err) {
+  //       console.log(err.response);
   //     })
   // }
-//}
+  completedItem = key => {
+    
+    const filteredItems = this.state.listItems.filter(item => {
+      return item.id !== key
+    })
+    this.setState({
+      key
+    })
+    request.put(`/api/tasks/${key}`, { completed: true})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        this.props.completedItems(res.data.task)
+        // this.setState({
+        //   key
+        // })
+        // window.location.reload(true);
+        // window.location.href='/tasks'
+      })
+      .catch(function (err) {
+        console.log(err.response);
+      })
+  }
+  
+
   deleteItem = key => {
     const filteredItems = this.state.listItems.filter(item => {
       return item.id !== key
@@ -96,26 +130,26 @@ class TaskList extends Component {
       <Fragment>
         <div>
         <ul>
-          {this.state.listItems.filter(item => item.completed == true).map(listitem => (
-            <li key={listitem.id}>
-              <input type="checkbox" />
-              {listitem.title} 
-              <Link to={`/tasks/${listitem.id}/show`} >Show</Link>
-              <Link to={`/tasks/${listitem.id}/edit`}>Edit</Link>
-              <button onClick={() => this.deleteItem(listitem.id)}>Delete</button>
-              <button onClick={() => this.completedItem(listitem.id)}>Completed</button>
-            </li>
-          ))}
-        </ul>
-        <hr/>
-        <ul>
-          {this.state.listItems.map(listitem => (
-            <li className="completed" key={listitem.id}> 
+          {this.state.listItems.filter(item => item.completed == false).map(listitem => (
+            <li  key={listitem.id}> 
               <input type="checkbox" />
               {listitem.title}
             <Link to={`/tasks/${listitem.id}/show`} >Show</Link>
             <Link to={`/tasks/${listitem.id}/edit`}>Edit</Link>
             <button onClick={() => this.deleteItem(listitem.id)}>Delete</button> 
+            <button  bsStyle="primary" onClick={(e) => this.completedItem(listitem.id)}>Completed</button>
+            </li>
+            ))}
+        </ul>
+        <hr/>
+        <ul>
+          {this.state.listItems.filter(item => item.completed == true).map(listitem => (
+            <li className="completed" key={listitem.id}>
+              <input type="checkbox" />
+              {listitem.title} 
+            <Link to={`/tasks/${listitem.id}/show`} >Show</Link>
+            <Link to={`/tasks/${listitem.id}/edit`}>Edit</Link>
+            <button onClick={() => this.deleteItem(listitem.id)}>Delete</button>
             </li>
             ))}
         </ul>
@@ -126,3 +160,4 @@ class TaskList extends Component {
 }
 
 export default TaskList;
+
